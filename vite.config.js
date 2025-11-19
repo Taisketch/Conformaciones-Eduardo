92d5d923851351572a4cb78a6adc4412bd2d4eb4
Eloy Garrido <eloegf@gmail.com>
2025-11-18 22:59:56 -0300

    preparado para despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Conformaciones-Eduardo/',
   plugins: [react()],
   server: {
     port: 5173,
